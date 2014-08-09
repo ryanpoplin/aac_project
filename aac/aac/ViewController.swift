4e34704cf37f2b4b566e51e91621149c0bf3100c
Byrdann Fox <mobileupstate@gmail.com>
2014-08-09 03:48:58 -0400

    Updates...

--- aac/aac/ViewController.swift
+++ aac/aac/ViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 // custom class of the UIViewController class...
 class ViewController: UIViewController {
     
+    /* LABEL OUTLETS... */
     // where am i supposed to put actions and outlets???...
     @IBOutlet weak var testLabel: UILabel!
     
@@ -23,9 +24,95 @@ class ViewController: UIViewController {
         // testing...
         // so it's [String] and not String[]...
         var arr: [String] = ["Ryan", "Poplin"]
-        print(arr)
+        println(arr)
         
-        testLabel.text = "Da Fuck Swift..."
+        /* GET ISSUE OUT OF THE COLOR COUNTDOWN APP FOR HOCKEYAPP... */
+        /* FIX THE STORYBOARD ISSUE AND THE SIMULATOR ISSUE HERE... */
+        
+        // some math...
+        
+        var firstValue = 2
+        var secondValue = 2
+        
+        // create visualizations for these...
+        var resultAdd = firstValue + secondValue
+        println(resultAdd)
+        
+        var resultSubtract = firstValue - secondValue
+        println(resultSubtract)
+        
+        var resultMultiply = firstValue * secondValue
+        println(resultMultiply)
+        
+        var resultDivide = firstValue / secondValue
+        println(resultDivide)
+        
+        // a string...
+        var testString = "Da fuck be up?"
+        
+        // the label object ref through an outlet...
+        testLabel.text = testString
+        
+        var index = 1
+        while index < 10
+        {
+            index += 1
+            println(index)
+        }
+     
+        // constants and variables...
+        let testConstant = 5
+        var testVariable = 24
+        
+        // variables: Int, Float, Double, String, and Bool...
+        // if you want to declare its type...
+        var intDemo:Int = 34
+        var floatDemo:Float = 34.5
+        var doubleDemo:Double = 45.5
+        var stringDemo:String = "Harry"
+        var boolDemo:Bool = false
+        
+        typealias percentageNumber = Float
+        typealias wholeNumber = Int
+        
+        // :...
+        var anotherSampleFloat:percentageNumber = 245.04
+        println(anotherSampleFloat)
+        
+        // Tuple...
+        // a tuple === (,)
+        let image = (600, 840)
+        let (height, width) = image
+        println("Height of image is \(height) and the width is \(width)!")
+        // fucking cool...
+        
+        var devices:String
+        devices = "iPhone 5c"
+        
+        let changeNot = "Constant"
+        var change = " Variable"
+        println("\(changeNot) or\(change)?")
+        // concatenation...
+        let quote = changeNot + change
+        println(quote)
+        
+        // arrays have changed...
+        // look up Swift array manipulation...
+        var deviceList:[String] = ["iPad","iPod","iMac","Macbook","iPhone"]
+        
+        // array === [1,2,3,4]
+        // dictionary === [key:val,key:val,key:val]
+        
+        // nice...
+        println(changeNot != change)
+        
+        // test AAC words array...or dictionary...
+        var arrayOfWords:[String] = ["I", "am", "sick"]
+        println(arrayOfWords[0])
+        println(arrayOfWords[1])
+        println(arrayOfWords[2])
+        
+        
         
     }
 
