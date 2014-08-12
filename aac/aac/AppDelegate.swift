d3fc6edf8a9d2f7ee6ebe55cdb28675c93305765
Byrdann Fox <mobileupstate@gmail.com>
2014-08-11 21:27:25 -0400

    Updates...

--- aac/aac/AppDelegate.swift
+++ aac/aac/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window!.backgroundColor = UIColor.whiteColor()
+        window!.rootViewController = ViewController()
+        window!.makeKeyAndVisible()
+        
         // Override point for customization after application launch.
         return true
     }
--- aac/aac/ViewController.swift
+++ aac/aac/ViewController.swift
@@ -6,26 +6,45 @@
 //  Copyright (c) 2014 ExcepApps, Inc. All rights reserved.
 //
 
-// import...
 import UIKit
+import QuartzCore
 
 // custom class of the UIViewController class...
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     
-    // where am i supposed to put actions and outlets???...
+    var collectionView: UICollectionView?
     
-    // override?...
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 90, height: 120)
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView!)
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
+        cell.backgroundColor = UIColor.whiteColor()
+        cell.layer.borderWidth = 0.5
+        cell.layer.borderColor = (UIColor( red: 0.5, green: 0.5, blue:0, alpha: 1.0 )).CGColor
+        return cell
     }
 
 }
